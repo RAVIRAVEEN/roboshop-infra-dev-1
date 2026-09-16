a483b83527338d23f3ccd7db27373658ad2d2e90
RAVIRAVEEN <raviraveen7@gmail.com>
2026-09-16 09:55:50 +0530

    roboshop-infra-dev.

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -88,3 +88,93 @@ provisioner "remote-exec" {
     ]
   }
 }
+
+
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge(
+    local.common_tags,
+  
+         {
+        Name = "${local.common_names_suffix}-rabbitmq"
+    }
+  
+  )
+
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+  aws_instance.rabbitmq.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh rabbitmq"
+
+    ]
+  }
+}
+
+
+
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge(
+    local.common_tags,
+  
+         {
+        Name = "${local.common_names_suffix}-mysql"
+    }
+  
+  )
+
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+  aws_instance.mysql.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh mysql"
+
+    ]
+  }
+}
