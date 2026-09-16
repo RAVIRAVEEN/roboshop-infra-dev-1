b4eb5876f79d27e87b7f451a612bcf1b54c26675
RAVIRAVEEN <raviraveen7@gmail.com>
2026-09-16 22:43:36 +0530

    roboshop-infra-dev.

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,7 +5,7 @@ sudo dnf install ansible -y
 #  environment=$2
 # ansible-pull -U https://github.com/RAVIRAVEEN/roboshop-ansible-roles-tf-1.git -e component=$component main.yaml
 
-REPO_URl=https://github.com/RAVIRAVEEN/roboshop-ansible-roles-tf-1.git
+REPO_URl="https://github.com/RAVIRAVEEN/roboshop-ansible-roles-tf-1.git"
 REPO_DIR="opt/roboshop/ansible"
 ANSIBLE_DIR="roboshop-ansible-roles-tf-1"
 
