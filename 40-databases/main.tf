4c1076b049474bd795d0b5a880017b4e3fed3ebf
RAVIRAVEEN <raviraveen7@gmail.com>
2026-09-16 23:43:56 +0530

    roboshop-infra-dev.

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -46,48 +46,48 @@ provisioner "remote-exec" {
 }
 
 
-# resource "aws_instance" "redis" {
-#   ami           = local.ami_id
-#   instance_type = var.instance_type
-#   vpc_security_group_ids = [local.redis_sg_id]
-#   subnet_id = local.database_subnet_ids
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id = local.database_subnet_ids
 
-#   tags = merge(
-#     local.common_tags,
+  tags = merge(
+    local.common_tags,
   
-#          {
-#         Name = "${local.common_names_suffix}-redis"
-#     }
+         {
+        Name = "${local.common_names_suffix}-redis"
+    }
   
-#   )
+  )
 
-# }
+}
 
-# resource "terraform_data" "redis" {
-#   triggers_replace = [
-#   aws_instance.redis.id
-#   ]
+resource "terraform_data" "redis" {
+  triggers_replace = [
+  aws_instance.redis.id
+  ]
 
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.redis.private_ip
-#   }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
 
-#   provisioner "file" {
-#     source = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
-#   }
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
 
-# provisioner "remote-exec" {
-#     inline = [
-#       "sudo chmod +x /tmp/bootstrap.sh",
-#       "sudo sh /tmp/bootstrap.sh redis"
+provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis"
 
-#     ]
-#   }
-# }
+    ]
+  }
+}
 
 
 
