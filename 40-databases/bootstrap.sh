9a761e68e252a9888a0c6a97377f62ed198ccc0e
RAVIRAVEEN <raviraveen7@gmail.com>
2026-09-16 09:27:27 +0530

    roboshop-infra-dev.

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,5 +2,5 @@
 
 sudo dnf install ansible-core -y
 
-# component=$1
-ansible-pull -U https://github.com/RAVIRAVEEN/roboshop-ansible-roles-tf-1.git -e component=mongodb main.yaml
+ component=$1
+ansible-pull -U https://github.com/RAVIRAVEEN/roboshop-ansible-roles-tf-1.git -e component=$component main.yaml
